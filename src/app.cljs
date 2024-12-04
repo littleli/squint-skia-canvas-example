7ccd09b2269b2a2afd2771fb2c156f28f423cace
Ales Najmann <ales.najmann@omio.com>
2024-12-04 12:08:01 +0100

    Code reformat, gitignore update

--- src/app.cljs
+++ src/app.cljs
@@ -5,16 +5,15 @@
 
 (defn- paint-job [e]
   (let [ctx (.getContext (.-target.canvas e) "2d")]
-    (set! (.-lineWidth ctx)
-          (+ 25 (* 25 (Math/cos (/ e.frame 10)))))
+    (set! (.-lineWidth ctx) (+ 25 (* 25 (Math/cos (/ e.frame 10)))))
     (doto ctx
-        (.beginPath)
-        (.arc 150 150 50 0 (* 2 Math/PI))
-        (.stroke)
-        (.beginPath)
-        (.arc 150 150 10 0 (* 2 Math/PI))
-        (.stroke)
-        (.fill))))
+      (.beginPath)
+      (.arc 150 150 50 0 (* 2 Math/PI))
+      (.stroke)
+      (.beginPath)
+      (.arc 150 150 10 0 (* 2 Math/PI))
+      (.stroke)
+      (.fill))))
 
 (doto win
   (.-title "Squint Canvas Window")
