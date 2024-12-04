22fbb3a1cdd0baaa2c1973771c77a4eafb856e6a
Ales Najmann <ales.najmann@omio.com>
2024-12-04 11:39:15 +0100

    Add example code

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # squint-skia-canvas-example
 Skia Canvas use from Squint Code example
+
+To dev install babashka and run:
+
+```sh
+bb dev
+```
---
+++ src/app.cljs
@@ -0,0 +1,21 @@
+(ns app
+  (:require ["skia-canvas" :refer [Window]]))
+
+(def win (Window. 300 300))
+
+(defn- paint-job [e]
+  (let [ctx (.getContext (.-target.canvas e) "2d")]
+    (set! (.-lineWidth ctx)
+          (+ 25 (* 25 (Math/cos (/ e.frame 10)))))
+    (doto ctx
+        (.beginPath)
+        (.arc 150 150 50 0 (* 2 Math/PI))
+        (.stroke)
+        (.beginPath)
+        (.arc 150 150 10 0 (* 2 Math/PI))
+        (.stroke)
+        (.fill))))
+
+(doto win
+  (.-title "Squint Canvas Window")
+  (.on "draw" paint-job))
